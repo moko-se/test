3872a8eeef6158bf2ac580f7322357e15658e259
Moko-se <sergenzuzi@outlook.fr>
2022-03-08 10:39:08 +0100

    data.js

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const data = [{
+    id: 1,
+    pseudo: 'serge',
+    email: 'serge@doretdevins.com',
+    product: 'Vins Bio'
+}]
+
+const list = document.querySelector('#list');
+
+const pseudo_ = document.createElement('li');
+const email_ = document.createElement('li');
+const product_ = document.createElement('li');
+
+data.forEach(item => {
+    pseudo_.textContent = item.pseudo;
+    email_.textContent = item.email;
+    product_.textContent = item.product;
+
+    list.appendChild(pseudo_);
+    list.appendChild(email_);
+    list.appendChild(product_);
+})
